21ebda11dc18915258aec6378da70cfdd33cb192
Agathe Badia <ab@honeypot.io>
2020-03-05 16:00:25 +0100

    Change image size

---
+++ app/components/ui-form/input/component.js
@@ -0,0 +1,5 @@
+//Want reuse existing Ember input in this file
+
+import Textfield from '@ember/component/text-field';
+
+export default Textfield;
--- app/router.js
+++ app/router.js
@@ -9,4 +9,5 @@ export default class Router extends EmberRouter {
 Router.map(function() {
   this.route('home', { path: '/'});
   this.route('stories');
+  this.route('share');
 });
